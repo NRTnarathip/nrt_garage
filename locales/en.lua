7cd725dedf2a60c8b0a09e626f7994ae5c5dea53
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2021-02-17 00:01:09 +0700

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,5 +1,5 @@
 Locales={}
 Locales['en'] = {
-	['cannot_save_vehicle_garage'] = 'Cant\' save vehicle. Because you dont own this car',
+	['cannot_save_vehicle_garage'] = 'Can\'t save vehicle. Because you dont own this car',
 	['car_emty'] = 'Car is emty'
 }
